2f443278faa9de3ed1f111577238e623fc7cc751
InesDennehy <31110950+InesDennehy@users.noreply.github.com>
2018-09-25 23:14:25 -0300

    a

--- src/banco/LoginWindow.java
+++ src/banco/LoginWindow.java
@@ -1,7 +1,9 @@
 package banco;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -15,12 +17,14 @@ import javax.swing.JButton;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class LoginWindow extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField tfUser;
-	private JTextField tfPassword;
+	private JCTextField tfUser;
+	private JCTextField tfPassword;
 	private JButton btnOk;
 	private JButton btnDesconectar;
 
@@ -44,11 +48,14 @@ public class LoginWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginWindow() {
+		this.setTitle("BD Bank");
+		this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 300, 220);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new GridBagLayout());
+		contentPane.setBackground(new Color(247, 247, 247));
 		setContentPane(contentPane);
 		
 		GridBagConstraints c = new GridBagConstraints();
@@ -58,25 +65,61 @@ public class LoginWindow extends JFrame {
 		c.ipadx = 30;
 		c.ipady = 5;
 		c.gridwidth = 2;
-		c.insets = new Insets(5,10,5,10);
-		
-		tfUser = new JTextField();
-		contentPane.add(tfUser, c);
-		tfUser.setColumns(10);
+		c.insets = new Insets(0,15,0,10);
+		JLabel lblLogin = new JLabel("BD Bank - Login");
+		lblLogin.setFont(new Font("Segan-Light", Font.PLAIN, 14));
+		contentPane.add(lblLogin, c);
 		
 		c.gridy = 1;
+		c.insets = new Insets(5,10,5,10);
 		
-		tfPassword = new JTextField();
-		contentPane.add(tfPassword, c);
-		tfPassword.setColumns(10);
+		tfUser = new JCTextField();
+		contentPane.add(tfUser, c);
+		tfUser.setColumns(10);
+		tfUser.setPlaceholder("Usuario");
+		
+		tfUser.addFocusListener(new FocusListener() {
+
+		    @Override
+		    public void focusGained(FocusEvent e) {
+		    	tfUser.setBackground(new Color(255, 251, 229));
+		    }
+
+		    @Override
+		    public void focusLost(FocusEvent e) {
+		        tfUser.setBackground(Color.white);
+		    }
+
+		});
 		
 		c.gridy = 2;
+		
+		tfPassword = new JCTextField();
+		contentPane.add(tfPassword, c);
+		tfPassword.setColumns(10);
+		tfPassword.setPlaceholder("Contraseña");
+		tfPassword.addFocusListener(new FocusListener() {
+
+		    @Override
+		    public void focusGained(FocusEvent e) {
+		    	tfPassword.setBackground(new Color(255, 251, 229));
+		    }
+
+		    @Override
+		    public void focusLost(FocusEvent e) {
+		        tfPassword.setBackground(Color.white);
+		    }
+
+		});
+		
+		c.gridy = 3;
 		c.ipadx = 0;
 		c.ipady = 0;
 		c.gridwidth = 1;
-		c.weightx = 0.7;
+		c.weightx = 0.8;
 		c.insets = new Insets(0,10,0,0);
 		
+		//Color de boton 153, 167, 255
 		btnOk = new JButton("OK");
 		btnOk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -93,7 +136,7 @@ public class LoginWindow extends JFrame {
 		
 		c.gridx = 1;
 		c.insets = new Insets(0,0,0,10);
-		c.weightx = 0.3;
+		c.weightx = 0.2;
 		
 		btnDesconectar = new JButton("Desconectar");
 		btnDesconectar.addActionListener(new ActionListener() {
